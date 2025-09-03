6c971e743f5d63d1a4c06d0a5bd63b9b6ae14f02
Jason Whittle <jason.j.whittle@gmail.com>
2025-09-03 13:35:56 +1000

    common js file

---
+++ jw1_mpage_common.js
@@ -0,0 +1,29 @@
+$.extend({
+    reInitPopUps : function( id ){
+        //add the class of hover to all elements with the class of col2
+        $("#"+id+" .col2").hover(
+        function(){
+            if (this.title > " "){
+                this.tip = this.title;
+                this.title = "";
+                $(['<div id="tooltip">',this.tip,'</div>'].join("")).css( {
+                position: 'absolute',
+                width: '400px',
+                top: event.clientY+20,
+                left: event.clientX+40,
+                'font-weight': 'bold',
+                border: '1px solid #000',
+                padding: '10px 10px 10px 10px',
+                'background-color': '#ffc',
+                opacity: 1.00
+                }).appendTo("body").fadeIn(0);	
+            }      
+        },
+        function(){
+            if (this.tip > " "){
+                $("#tooltip").remove(); 
+                this.title = this.tip;
+            }
+        });	
+    }});
+    
